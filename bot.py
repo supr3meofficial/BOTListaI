dfb6ed1b011eba8e4309c0caa481ddd7df36fb1d
supr3meofficial <38699638+supr3meofficial@users.noreply.github.com>
2020-09-05 19:56:38 +0100

    Nova mensagem de bem-vinda a novos membros

--- bot.py
+++ bot.py
@@ -7,8 +7,18 @@ bot = commands.Bot(command_prefix='.')
 async def on_ready():
     print(f'BOT conectado como {bot.user}')
 
+# Reage a entradas de membros no servidor
+@bot.event
+async def on_member_join(member):
+    # Canal das bem-vindas definido como o canal de sistema
+    channel = member.guild.system_channel
+    print("Something at least happened")
+    if channel is not None:
+        await channel.send(f'Bem-vindo/a à família, {member.mention}. :purple_heart:')
+
 @bot.command()
 async def teste(ctx):
     await ctx.send('Parece estar a funcionar.')
+
 
 bot.run(os.environ["BOT_TOKEN"])

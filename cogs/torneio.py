102ad50bd1ff5043eefb35ee51af2f799a0e7c78
supr3meofficial <38699638+supr3meofficial@users.noreply.github.com>
2020-09-09 00:54:31 +0100

    Adicionei um check ao comando 'participar'

--- cogs/torneio.py
+++ cogs/torneio.py
@@ -6,7 +6,11 @@ class Torneio(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def check_channel_id(ctx):
+        return ctx.message.channel.id == 751866032133636188
+
     @commands.command()
+    @commands.check(check_channel_id)
     async def participar(self, ctx):
         await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
 

bb989ae4f6cefc0cb198ead7a4beaf045bdc7447
supr3meofficial <38699638+supr3meofficial@users.noreply.github.com>
2020-09-17 16:39:18 +0100

    Novo comando para Regras

--- cogs/etc.py
+++ cogs/etc.py
@@ -46,6 +46,45 @@ class AboutCog(commands.Cog):
         await ctx.send(embed=embed)
         embed=discord.Embed(title="<:PayPal:437213765923241986> Paypal Link", url="https://www.paypal.me/supr3medonate", description="Se quiseres ajudar o meu programador, aqui está o link <3", color=0x933db8)
         await ctx.send(embed=embed)
+    
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def regras(self, ctx):
+        rules = """
+
+**1 ❱ ‎ Não sejas rude com ninguém.**
+Não há necessidade nenhuma em insultar alguém.
+
+**2 ❱ ‎ Não sejas um distúrbio**
+Ninguém gosta de ter a sua conversa perturbada.
+
+**3 ❱ ‎ Não publiques conteúdo NSFW — coisas sangrentas or pornografia.**
+Mantém um chat amigável para todos.
+
+**4 ❱ ‎ Não publicites nada sem aprovação de um moderador.**
+Não sejas **esse** tipo.
+
+**5 ❱ ‎ Não spames o chat.**
+Vais ser mutado.
+
+**6 ❱ ‎ ‎Utiliza sempre os canais apropriados.**
+Estão lá por algum motivo.
+
+**7 ❱ ‎ ‎Segue os Termos e Condições do Discord.**
+Estão anexados em baixo.
+
+> Os moderadores deste discord reservam o direito de agir conforme achem correto sem aviso prévio.
+> Para apelar um ban contacta o <@203299786382639104>
+"""
+        await ctx.message.delete()
+        embed = discord.Embed(title='', description=rules, colour=0x933db8)
+        embed.set_author(icon_url=ctx.guild.icon_url, name='Regras do Servidor')
+        await ctx.send(embed=embed)
+        embed=discord.Embed(title='<:discord:434011189656158219> Termos de Serviço', url='https://discord.com/terms', description='', color=0x933db8)
+        await ctx.send(embed=embed)
+        embed=discord.Embed(title='<:discord:434011189656158219> Diretrizes', url='https://discord.com/guidelines', description='', color=0x933db8)
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(AboutCog(bot))
